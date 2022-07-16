a9ec6e395f2dc1b1e1a30951b521ad2c28ff9c68
Oleg Chaika <grozmaster@ukr.net>
2022-07-16 14:05:26 +0300

    autoplay slider

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(function () {
     $('.bike__slider').slick({
         arrows: false,
         dots:true,
-        fade:true
+        fade:true,
+        autoplay:true,
+        autoplaySpeed: 4000,
     });
 });
